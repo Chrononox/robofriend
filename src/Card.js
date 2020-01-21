4fbb067f511bc42262eb20664b053de74c582584
Travis <tegbert@gmail.com>
2020-01-21 17:45:32 -0600

    cat friends

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Card = ({name, email, id, username}) =>{
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src={`https://www.robohash.org/${username}?200x200`} /> 
+            <img alt='robots' src={`https://www.robohash.org/${username}?set=set4`} /> 
             <div>
             
                 <h2>{name}</h2>
