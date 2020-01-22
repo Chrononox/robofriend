909443fad7a326fb81e60c864c6cba82ad8d16d6
Travis <tegbert@gmail.com>
2020-01-21 20:28:00 -0600

    removed unneeded import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+// import Card from './Card';
 import Cardlist from './Cardlist';
 import  { robots }  from './robots';
 import * as serviceWorker from './serviceWorker';
