0dbfb2026a1f35848ee358e01147f93891ba9739
Travis <tegbert@gmail.com>
2020-01-22 11:14:12 -0600

    refactored

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cardlist from './Cardlist';
+import { robots } from './robots';
+
+const App = () => {
+    return(
+        <div>
+            <h1>RoboFriends</h1>
+            <Cardlist robots={robots}/>
+        </div>
+    )
+}
+
+export default App;
