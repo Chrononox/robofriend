239dd59caf68ad8f39d63aecab448f79a21cda35
Travis <tegbert@gmail.com>
2020-01-23 10:49:58 -0600

    using jsonplaceholder for user info now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Cardlist from './Cardlist';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+// import { robots } from './robots';
 import './App.css';
 
 
@@ -9,9 +9,17 @@ class App extends Component {
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
         }
+    }
+
+    componentDidMount(){
+        
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+
     }
 
     onSearchChange =(event) =>{
@@ -22,13 +30,18 @@ class App extends Component {
         const filterdRobots = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return(
-            <div className='tc'>
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <Cardlist robots={filterdRobots}/>
-            </div>
-        )
+        if (this.state.robots.length === 0){
+            return <h1>Loading...</h1>
+        } else {
+            return(
+                <div className='tc'>
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Cardlist robots={filterdRobots}/>
+                </div>
+            )
+        }
+        
     }
 
     
